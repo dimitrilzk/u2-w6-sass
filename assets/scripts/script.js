aeb7d875cfb4c5f43fcde8a333046c9145003597
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-05 16:58:26 +0200

    finito

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,6 @@
 function cambiaSfondo() {
   document.body.style.backgroundColor = "aqua";
 }
+function inserisciImmagine() {
+  document.body.innerHTML += `<div> <img src="https://i.picsum.photos/id/1021/2048/1206.jpg?hmac=fqT2NWHx783Pily1V_39ug_GFH1A4GlbmOMu8NWB3Ts" /> </div>`;
+}
